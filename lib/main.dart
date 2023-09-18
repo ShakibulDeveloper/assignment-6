ea13eaff84c74844aa6156674318cec8ff3eb1c3
ShakibulDeveloper <epiktheme@gmail.com>
2023-09-18 13:30:57 +0600

    Assignment6

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,242 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
+      title: 'Photo App',
     );
   }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class HomeScreen extends StatelessWidget{
+  //Custom Message PopUp
+  mySnackbarMgs(context, message){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Photo Gallery"),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ListTile(
+              title: Text("Welcome to My Photo Gallery!", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search for Photos...",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 1),
+                  ),
+                ),
+              ),
             ),
+            SizedBox(
+              height: 250,
+              child: Padding(
+                padding: const EdgeInsets.all(17.0),
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                        0, 0, 4, 0
+                      ),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 1 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                              fit: BoxFit.fill,
+                              height: 70,
+                            ),
+                          ),
+                          Text("Photo 01", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          0, 0, 4, 0
+                      ),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 2 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1682686580224-cd46ea1a6950?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                              fit: BoxFit.cover,
+                              height: 70,
+                            ),
+                          ),
+
+                          Text("Photo 02", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 3 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1694746027334-66d47a21d645?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                              fit: BoxFit.cover,
+                              height: 70,
+                            ),
+                          ),
+                          Text("Photo 03", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          0, 4, 4, 0
+                      ),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 4 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1694279873840-ae24c33e3359?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80",
+                              fit: BoxFit.cover,
+                              height: 70,
+                            ),
+                          ),
+                          Text("Photo 04", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          0, 4, 4, 0
+                      ),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 5 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1683009427666-340595e57e43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                              fit: BoxFit.cover,
+                              height: 70,
+                            ),
+                          ),
+                          Text("Photo 05", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                          0, 4, 0, 0
+                      ),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap:(){mySnackbarMgs(context, "Photo 6 Clicked!");},
+                            child: Image.network("https://images.unsplash.com/photo-1693788452415-705a7f6d1d5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80",
+                              fit: BoxFit.cover,
+                              height: 70,
+                            ),
+                          ),
+
+                          Text("Photo 06", style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: ClipOval(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1443&q=80',
+                  fit: BoxFit.cover,
+                  height: 50,
+                  width: 50,
+                ),
+              ),
+              title: Text("Photo 1", style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text("Description for Photo 1"),
+            ),
+            ListTile(
+              leading: ClipOval(
+                child: Image.network(
+                  'https://images.unsplash.com/photo-1574158622682-e40e69881006?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80',
+                  fit: BoxFit.cover,
+                  height: 50,
+                  width: 50,
+                ),
+              ),
+              title: Text("Photo 2", style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text("Description for Photo 2"),
+            ),
+            ListTile(
+              leading: ClipOval(
+                child: Image.network(
+                  'https://plus.unsplash.com/premium_photo-1677101221533-52b45823a2dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80',
+                  fit: BoxFit.cover,
+                  height: 50,
+                  width: 50,
+                ),
+              ),
+              title: Text("Photo 3", style: TextStyle(fontWeight: FontWeight.bold),),
+              subtitle: Text("Description for Photo 3"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                bottom: 20,
+              ),
+              child: Column(
+                children: [
+                  IconButton(onPressed: (){mySnackbarMgs(context, "Photos Uploaded Successfully!");}, icon: Icon(Icons.add_circle,
+                    size: 42,
+                    color: Colors.blue,
+                  )),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
